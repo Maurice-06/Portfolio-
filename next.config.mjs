6a8e0f91876cf3ee39d958f59120c0a701630cd1
Maurice Birame Diouf <mauricebiramediouf@mac.home>
2025-12-11 15:20:26 +0000

    Final: Correction basePath et output pour déploiement statique GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,9 @@
 const nextConfig = {
   basePath: '/Portfolio-',
   output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+};
+
 module.exports = nextConfig;
 
-export default nextConfig
 
 
