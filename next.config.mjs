329c276834d70f1d5d834068813594030be27655
Maurice Birame Diouf <mauricebiramediouf@mac.home>
2025-12-11 15:06:24 +0000

    Config: Ajout de basePath pour le déploiement sur GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/Portfolio-',
   output: 'export',
   typescript: {
     ignoreBuildErrors: true,
@@ -8,5 +9,8 @@ const nextConfig = {
     unoptimized: true,
   },
 }
+module.exports = nextConfig;
 
 export default nextConfig
+
+
